04d04358ab0d26b0562c469b22f18e5215f39a90
Michael Gutteridge <mrg@fhcrc.org>
2016-10-27 10:49:07 -0700

    added bootstrap attribute for config

--- attributes/consul.rb
+++ attributes/consul.rb
@@ -1,5 +1,6 @@
 default['svalbard-vault']['consul']['version'] = '0.7.0'
 default['svalbard-vault']['consul']['config'] = {
   'data_dir' => '/var/spool/consul/data',
-  'dc' => 'e2'
+  'dc' => 'e2',
+  'bootstrap' => 'false'
 }
